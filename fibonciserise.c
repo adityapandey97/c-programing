6648ecf9edf3a6db71408a47ef3e71b368d2e199
Aditya pandey <honeypandey409@gmail.com>
2025-01-09 00:46:19 +0530

    Create fibonciserise.c

---
+++ fibonciserise.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void fibonaciSeries(int){
+    for(int i=1; i<=n;i++){
+        printf()
+    }
+}
+int main() {
+  int n;
+  printf("Enter no. of term");
+  
+
+    return 0;
+}
