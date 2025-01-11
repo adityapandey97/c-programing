6d0742b54343d6f4f7c61ee36dd5b01daf603231
Aditya pandey <honeypandey409@gmail.com>
2025-01-11 11:48:06 +0530

    Create arrayInputs.c

---
+++ arrayInputs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    printf("enter the number;");
+    int n,a=0;
+    scanf("%d",&n);
+    int arr [n];
+   for (int i = 0; i < n; i++)
+   {printf("value of arr:");
+    scanf("%d",&arr[i]);
+   }
+    printf("array is:");
+    for (int i = 0; i < n; i++)
+     {
+       printf("d",arr[i]);
+     }
+        printf("array is:");
+       }
